c55b2b3c898cd8f1a0076b51a5d7de0ab5809231
ViVse <ivanisenko730@gmail.com>
2023-04-27 14:41:57 +0300

    made email not required

--- backend/src/validators/user-validator.js
+++ backend/src/validators/user-validator.js
@@ -2,6 +2,6 @@ import Joi from "joi";
 
 export const userPutSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
+  email: Joi.string(),
   password: Joi.string().trim().min(6).max(20),
 });
