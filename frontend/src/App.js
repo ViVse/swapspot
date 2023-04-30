8e0666dbe69298f815dd1b26d56c43a7afbd7f7d
ViVse <ivanisenko730@gmail.com>
2023-05-01 00:48:26 +0300

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import Header from "./components/Layout/Header";
 import Home from "./pages/Home";
+import Footer from "./components/Layout/Footer";
 
 function App() {
   return (
-    <div className="container mx-auto px-4">
-      <Header />
-      <Home />
-    </div>
+    <>
+      <div className="container mx-auto px-4">
+        <Header />
+        <Home />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Layout/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <footer className="bg-teal-900 text-white mt-20 py-10">
+    <div className="container mx-auto px-4">
+      <p>©2023 SwapSpot. All rights reserved.</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
