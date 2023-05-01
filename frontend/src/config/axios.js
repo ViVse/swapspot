a2e59a790c51d410362167926fe88313a8447885
ViVse <ivanisenko730@gmail.com>
2023-05-01 22:58:46 +0300

    header for auth

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import { getCookie } from "../utils/cookie";
 
 export default axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
   withCredentials: true,
+  headers: {
+    "x-auth-token": getCookie("x-auth-token"),
+  },
 });
