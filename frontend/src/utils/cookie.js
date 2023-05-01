7042045f5d957478e876b55ee6d337933e5762c3
ViVse <ivanisenko730@gmail.com>
2023-05-01 22:57:50 +0300

    functions to work with cookies

---
+++ frontend/src/utils/cookie.js
@@ -0,0 +1,8 @@
+export const deleteCookie = (name) => {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+};
+
+export const getCookie = (name) => {
+  var match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
+  if (match) return match[2];
+};
