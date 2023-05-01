9aa903a5f5fd79ef85c46d4b0984d0b8ed4ee225
ViVse <ivanisenko730@gmail.com>
2023-05-01 15:19:58 +0300

    added use-input hook

--- frontend/src/components/UI/Input.js
+++ frontend/src/components/UI/Input.js
@@ -5,7 +5,7 @@ const Input = (props) => {
     <div className={props.className}>
       <label className="text-xl block font-medium text-teal-900">{label}</label>
       <input
-        className="rounded-lg bg-gray-50 border border-solid h-10 w-60 px-2 border-teal-900 focus-visin:outline-teal-900"
+        className="rounded-lg w-full bg-gray-50 border border-solid h-10 px-2 border-teal-900 focus-visin:outline-teal-900"
         type={type}
         name={name}
         value={value}
--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,5 +1,89 @@
+import useInput from "../../hooks/use-input";
+import { emailRule } from "../../utils/inputValidationRules";
+import Input from "../../components/UI/Input";
+import { Button } from "flowbite-react";
+import { notEmptyRule } from "../../utils/inputValidationRules";
+import { IoLogoGoogle } from "react-icons/io";
+
+import styles from "./Login.module.scss";
+
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(emailRule);
+
+  const {
+    value: passwordValue,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPassword,
+  } = useInput(notEmptyRule);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!emailIsValid || !passwordIsValid) return;
+
+    resetEmail();
+    resetPassword();
+  };
+
+  return (
+    <div
+      className={`${styles.Login} bg-gradient-to-r from-green-400 to-teal-900 flex justify-center items-center`}>
+      <form
+        className="bg-white py-14 px-24 rounded-xl"
+        onSubmit={submitHandler}>
+        <h1 className="text-center text-3xl text-teal-900 font-bold">Вхід</h1>
+        <div className="mt-3">
+          <Input
+            label="Email"
+            value={emailValue}
+            hasError={emailHasError}
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+          />
+          {emailHasError && (
+            <span className="block text-red-800">Введіть правильну пошту</span>
+          )}
+        </div>
+        <div className="mt-3">
+          <Input
+            className="w-72"
+            label="Пароль"
+            value={passwordValue}
+            hasError={passwordHasError}
+            onChange={passwordChangeHandler}
+            onBlur={passwordBlurHandler}
+          />
+          {passwordHasError && (
+            <span className="block text-red-800">
+              Пароль не може бути пустим
+            </span>
+          )}
+        </div>
+        <div className="flex flex-col justify-between">
+          <Button
+            className="bg-teal-900 font-medium hover:bg-teal-700 mt-3"
+            type="submit">
+            Увійти
+          </Button>
+          <Button
+            className="bg-white border border-solid border-teal-900 !text-teal-900 font-medium hover:bg-green-300 mt-3"
+            type="button">
+            <IoLogoGoogle className="mr-2" /> Увійти через Google
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
