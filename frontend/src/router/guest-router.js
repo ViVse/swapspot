4a7f0641ccdef245e2b59e7602bf8d84718a36a3
ViVse <ivanisenko730@gmail.com>
2023-05-02 05:21:30 +0300

    added search to guest router

--- frontend/src/router/guest-router.js
+++ frontend/src/router/guest-router.js
@@ -3,11 +3,16 @@ import { redirect, createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Layout from "../components/Layout/Layout";
+import Search from "../pages/Search/Search";
 
 const guestRouter = createBrowserRouter([
   {
     element: <Layout />,
     children: [
+      {
+        path: "/search",
+        element: <Search />,
+      },
       {
         path: "/",
         element: <Home />,
