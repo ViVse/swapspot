1e48edea0667bc402762fc52668ac0833b5e3eb1
ViVse <ivanisenko730@gmail.com>
2023-05-13 03:29:05 +0300

    minLength validation

--- frontend/src/utils/inputValidationRules.js
+++ frontend/src/utils/inputValidationRules.js
@@ -1,3 +1,7 @@
 export const notEmptyRule = (value) => value.trim() !== "";
 
 export const emailRule = (value) => value.includes("@");
+
+export const minLengthRule = (length) => {
+  return (value) => value.trim().length >= length;
+};
