a6017857e8c8c57dc1fe3b7418330b6d622789bc
ViVse <ivanisenko730@gmail.com>
2023-05-13 20:28:25 +0300

    Product page

---
+++ frontend/src/pages/Product/Product.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "../../config/axios";
+import Spinner from "../../components/UI/Spinner";
+import { Badge, Button } from "flowbite-react";
+import { Gallery } from "../../components/UI/Gallery/Gallery";
+
+const Product = (props) => {
+  const { id } = useParams();
+  const [product, setProduct] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`/api/products/${id}`)
+      .then((res) => setProduct(res.data))
+      .catch(() => setProduct(null))
+      .finally(() => setIsLoading(false));
+  }, [id]);
+
+  if (isLoading) {
+    return (
+      <section className="container mx-auto px-4 mb-20 pt-14 flex flex-col items-center">
+        <h1 className="font-bold text-xl mb-3">Завантаження...</h1>
+        <Spinner />
+      </section>
+    );
+  }
+
+  if (!product) {
+    return (
+      <section className="container mx-auto px-4 mb-20 flex flex-col items-center">
+        <h1 className="font-bold text-2xl mt-5 mb-3">
+          Не можемо знайти ваш продукт
+        </h1>
+        <Link to="/">
+          <Button color="success">На головну</Button>
+        </Link>
+      </section>
+    );
+  }
+
+  return (
+    <section className="container mx-auto px-4 mb-20">
+      <div className="flex">
+        <Gallery
+          className="w-1/2"
+          imgs={product.imgs.map((img) => img.publicUrl)}
+        />
+        <div className="w-1/2 ml-5 mt-3">
+          <h1 className="font-bold text-4xl">{product.name}</h1>
+          <div className="flex flex-wrap">
+            {product.tags.map((tag) => (
+              <Badge key={tag} color="success" className="mr-3 mt-4">
+                <span className="text-base font-bold px-3">{tag}</span>
+              </Badge>
+            ))}
+          </div>
+          <div className="flex mt-6 items-center">
+            <img
+              className="w-16 h-16 rounded-full object-cover mr-2"
+              src={product.owner.avatar.publicUrl}
+              alt={product.owner.name}
+            />
+            <div>
+              <h3 className="text-lg font-medium">{product.owner.name}</h3>
+              <span className="text-base text-gray-500">
+                На SwapSpot з{" "}
+                {new Date(product.owner.createdAt).toLocaleDateString()}
+              </span>
+            </div>
+          </div>
+          <div className="mt-2">
+            <Link
+              className="underline text-gray-500 font-medium"
+              to={`/products/user/${product.owner._id}`}>
+              Усі оголошення користувача
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
