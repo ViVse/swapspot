4ef45f5233de8b2eed50e42b6f6cf57597592aed
ViVse <ivanisenko730@gmail.com>
2023-05-13 21:33:44 +0300

    finished styling

--- frontend/src/pages/Product/Product.js
+++ frontend/src/pages/Product/Product.js
@@ -1,14 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
+import { getCookie } from "../../utils/cookie";
 import axios from "../../config/axios";
 import Spinner from "../../components/UI/Spinner";
 import { Badge, Button } from "flowbite-react";
 import { Gallery } from "../../components/UI/Gallery/Gallery";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import AuthContext from "../../store/auth-context";
 
 const Product = (props) => {
+  const { user, changeFavorites } = useContext(AuthContext);
   const { id } = useParams();
   const [product, setProduct] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(
+    user?.favorites.includes(id) || false
+  );
 
   useEffect(() => {
     setIsLoading(true);
@@ -18,6 +25,20 @@ const Product = (props) => {
       .catch(() => setProduct(null))
       .finally(() => setIsLoading(false));
   }, [id]);
+
+  const likeHandler = () => {
+    axios
+      .patch(`api/users/favorite/${id}`, {
+        headers: {
+          "x-auth-token": getCookie("x-auth-token"),
+        },
+      })
+      .then((res) => {
+        changeFavorites(id);
+        setIsFavorite((prev) => !prev);
+      })
+      .catch((err) => console.log(err));
+  };
 
   if (isLoading) {
     return (
@@ -53,7 +74,7 @@ const Product = (props) => {
           <div className="flex flex-wrap">
             {product.tags.map((tag) => (
               <Badge key={tag} color="success" className="mr-3 mt-4">
-                <span className="text-base font-bold px-3">{tag}</span>
+                <span className="text-sm font-bold px-3">{tag}</span>
               </Badge>
             ))}
           </div>
@@ -78,7 +99,42 @@ const Product = (props) => {
               Усі оголошення користувача
             </Link>
           </div>
+          {user && product.owner._id !== user._id && (
+            <div className="flex items-center mt-5">
+              {isFavorite ? (
+                <AiFillHeart
+                  className="fill-red-500 w-8 h-8 mr-4 cursor-pointer"
+                  onClick={likeHandler}
+                />
+              ) : (
+                <AiOutlineHeart
+                  className="fill-red-500 w-8 h-8 mr-4 cursor-pointer"
+                  onClick={likeHandler}
+                />
+              )}
+              <Button className="!border !border-solid !border-teal-900 bg-transparent !text-teal-900 !font-semibold hover:bg-green-300 mr-4">
+                Повідомлення
+              </Button>
+              <Button className="bg-teal-900 font-medium hover:bg-teal-700 mr-4">
+                Запропонувати обмін
+              </Button>
+            </div>
+          )}
+          <div className="mt-5">
+            <h3 className="inline font-semibold text-xl mr-2">
+              Місцезнаходження
+            </h3>
+            <span className="text-lg">{product.location}</span>
+          </div>
         </div>
+      </div>
+      <div>
+        <h2 className="font-semibold text-2xl">Опис</h2>
+        <p className="mt-2">{product.description}</p>
+        <p className="mt-2 text-sm text-gray-500">
+          Опубліковано: {new Date(product.createdAt).toLocaleDateString()}{" "}
+          {new Date(product.createdAt).toLocaleTimeString()}
+        </p>
       </div>
     </section>
   );
