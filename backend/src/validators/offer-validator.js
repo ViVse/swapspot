d885c651e014e58bd946f7ff94a2d75f28f07b7a
ViVse <ivanisenko730@gmail.com>
2023-05-15 16:34:07 +0300

    don't allow empty products

--- backend/src/validators/offer-validator.js
+++ backend/src/validators/offer-validator.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const offerSchema = Joi.object({
   to: {
     user: Joi.string().required(),
-    products: Joi.array().items(Joi.string()),
+    products: Joi.array().items(Joi.string()).min(1),
   },
-  products: Joi.array().items(Joi.string()),
+  products: Joi.array().items(Joi.string()).min(1),
 });
