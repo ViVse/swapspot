52dd910d27ff16ef46a9eeee3113059a140499f4
ViVse <ivanisenko730@gmail.com>
2023-05-16 03:15:43 +0300

    added more props

--- frontend/src/components/UI/Input.js
+++ frontend/src/components/UI/Input.js
@@ -1,16 +1,18 @@
 const Input = (props) => {
-  const { label, type, name, onChange, onBlur, value } = props;
+  const { label, placeholder, type, name, onChange, onBlur, value, required } = props;
 
   return (
     <div className={props.className}>
       <label className="text-xl block font-medium text-teal-900">{label}</label>
       <input
-        className="rounded-lg w-full bg-gray-50 border border-solid h-10 px-2 border-teal-900 focus-visin:outline-teal-900"
+        className="rounded-lg w-full bg-gray-50 border border-solid h-10 px-2 mt-2 border-teal-900 focus-visin:outline-teal-900"
         type={type}
+        placeholder={placeholder}
         name={name}
         value={value}
         onChange={onChange}
         onBlur={onBlur}
+        required={required}
       />
     </div>
   );
