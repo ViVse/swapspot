c259601a772982b3688cf632bdcd8354069277ea
ViVse <ivanisenko730@gmail.com>
2023-05-17 21:35:21 +0300

    added timestamps

--- backend/src/models/Offer.js
+++ backend/src/models/Offer.js
@@ -1,37 +1,42 @@
 import mongoose from "mongoose";
 import { OFFER_STATUS } from "../../../const/offerStatus.js";
 
-const offerSchema = new mongoose.Schema({
-  to: {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
+const offerSchema = new mongoose.Schema(
+  {
+    to: {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+      },
+      products: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: true,
+        ref: "Product",
+      },
     },
-    products: {
-      type: [mongoose.Schema.Types.ObjectId],
-      required: true,
-      ref: "Product",
+    from: {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+      },
+      products: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: true,
+        ref: "Product",
+      },
+    },
+    status: {
+      type: String,
+      required: [true, "can't be blank"],
+      enum: { values: OFFER_STATUS, message: "{VALUE} is not supported" },
     },
   },
-  from: {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
-    products: {
-      type: [mongoose.Schema.Types.ObjectId],
-      required: true,
-      ref: "Product",
-    },
-  },
-  status: {
-    type: String,
-    required: [true, "can't be blank"],
-    enum: { values: OFFER_STATUS, message: "{VALUE} is not supported" },
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Offer = mongoose.model("Offer", offerSchema);
 
