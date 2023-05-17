e8b0f88ad07a321af77d8db2817ffb0c4515525a
ViVse <ivanisenko730@gmail.com>
2023-05-17 17:36:02 +0300

    allowed links

--- backend/src/validators/notification-validator.js
+++ backend/src/validators/notification-validator.js
@@ -2,5 +2,6 @@ import Joi from "joi";
 
 export const notificationSchema = Joi.object({
   title: Joi.string().required(),
-  text: Joi.string().required()
-})
+  text: Joi.string().required(),
+  link: Joi.string(),
+});
