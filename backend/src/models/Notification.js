d784424b32df258bed16497f323d724d2f648e76
ViVse <ivanisenko730@gmail.com>
2023-05-17 15:58:24 +0300

    created Notification model

---
+++ backend/src/models/Notification.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Notificaton = mongoose.model("Notification", notificationSchema);
+
+export default Notificaton;
