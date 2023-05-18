76f6e742c7a9484fb94549a96ccb64d4af2cddfc
ViVse <ivanisenko730@gmail.com>
2023-05-18 17:44:47 +0300

    fetchUnreadCount

---
+++ frontend/src/store/chat-actions.js
@@ -0,0 +1,18 @@
+import axios from "../config/axios";
+import { getCookie } from "../utils/cookie";
+import { chatActions } from "./chat-slice";
+
+export const fetchUnreadCount = () => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.get("/api/chat/conversations/unread", {
+        headers: {
+          "x-auth-token": getCookie("x-auth-token"),
+        },
+      });
+      dispatch(chatActions.setUnreadCount(res.data.unreadCount));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
