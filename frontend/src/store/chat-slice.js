997ed0ebe6ee0a5677472e6996c8d1ed2734a3c0
ViVse <ivanisenko730@gmail.com>
2023-06-02 23:17:35 +0300

    actions for individual chat

--- frontend/src/store/chat-slice.js
+++ frontend/src/store/chat-slice.js
@@ -7,6 +7,7 @@ const chatSlice = createSlice({
     selectedConversation: "",
     // messages of selected conversation
     messages: [],
+    members: [],
     unreadCount: 0,
   },
   reducers: {
@@ -18,13 +19,32 @@ const chatSlice = createSlice({
     setUnreadCount(state, action) {
       state.unreadCount = action.payload;
     },
+    // action.payload: number to modify by
+    addUnread(state, action) {
+      state.unreadCount += action.payload;
+    },
     // action.payload: messages
     setMessages(state, action) {
       state.messages = action.payload;
     },
+    // action.pauload: message
+    addMessage(state, action) {
+      if (action.payload.conversation !== state.selectedConversation) {
+        state.unreadCount++;
+        return;
+      }
+      state.messages.push(action.payload);
+    },
     // action.payload: conversationId
     selectConversation(state, action) {
-      state.selectedConversation = action.payload;
+      state.selectedConversation = action.payload._id;
+      state.messages = action.payload.messages;
+      state.members = action.payload.members;
+    },
+    exitChat(state) {
+      state.selectedConversation = "";
+      state.members = [];
+      state.messages = [];
     },
   },
 });
